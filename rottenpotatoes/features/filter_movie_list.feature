822e25954a91e26bbba569546f1a3dbd01ba7934
Sam Biondolillo <biwinning121@gmail.com>
2016-07-05 18:49:57 +0000

    Filter Movie List Feature Passing

--- rottenpotatoes/features/filter_movie_list.feature
+++ rottenpotatoes/features/filter_movie_list.feature
@@ -23,10 +23,17 @@ Background: movies have been added to database
 
 Scenario: restrict to movies with 'PG' or 'R' ratings
   # enter step(s) to check the 'PG' and 'R' checkboxes
+  Given I check the following ratings: PG,R
   # enter step(s) to uncheck all other checkboxes
+  And I uncheck the following ratings: G,PG-13
   # enter step to "submit" the search form on the homepage
+  When I press "Refresh"
   # enter step(s) to ensure that PG and R movies are visible
+  Then I should see "The Terminator"
   # enter step(s) to ensure that other movies are not visible
-
+  And I should not see "Chicken Run"
+  
 Scenario: all ratings selected
-  # see assignment
+  Given I check the following ratings: G,PG,PG-13,R
+  When I press "Refresh"
+  Then I should see all the movies
--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -25,14 +25,25 @@ end
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
 
-When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+When /I check the following ratings: (.*)/ do |fields|
+  selected = fields.split(",")
+  selected.each do |field|
+    check("ratings_#{field}")
+  end
+end
+
+When /I uncheck the following ratings: (.*)/ do |fields|
   # HINT: use String#split to split up the rating_list, then
+  unselected = fields.split(",")
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  unselected.each do |field|
+    uncheck("ratings_#{field}")
+  end
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  rows = page.all('table#movies tr').count
+  expect(rows).to eq 11
 end
